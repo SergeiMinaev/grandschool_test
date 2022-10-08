5800420c11fbac345221f30b860ea95b4e7ebe3f
Sergei Minaev <brainsburn@yandex.ru>
2022-10-08 03:15:48 +0300

    Add  OnlineForm

--- src/About.vue
+++ src/About.vue
@@ -6,7 +6,7 @@
       <div class='about__docs about-docs'>
         <div class='about-docs__item' v-for='doc in docs'>
           <img :src='doc.thumb' />
-          <span>{{ doc.label }}&nbsp;→</span>
+          <span class='text-btn'>{{ doc.label }}&nbsp;→</span>
         </div>
       </div>
     </div>
@@ -25,6 +25,26 @@
       <h2 class='coop__title'>Сотрудничаем любым способом</h2>
       <div class='coop__cards'>
         <div class='coop__card'>
+          <OnlineForm class='coop-card__img' />
+          <div class='coop-card__link text-btn'>Онлайн&nbsp;заявка&nbsp;→</div>
+        </div>
+        <div class='coop__card coop-card'>
+          <img class='coop-card__img' src='./assets/img/call-us.jpg' />
+          <div class='coop-card__link text-btn'>Звонок&nbsp;нам&nbsp;→</div>
+        </div>
+        <div class='coop__card coop-card'>
+          <img class='coop-card__img' src='./assets/img/office.jpg' />
+          <div class='coop-card__link text-btn'>Посещение&nbsp;офиса&nbsp;→</div>
+        </div>
+      </div>
+    </div>
+
+    <div class='areas'>
+      <h2 class='areas__title'>Наши ресурсы</h2>
+      <div class='areas__items'>
+        <div class='areas__item' v-for='item in resources'>
+          <span v-html='item.label'></span>
+          <Btn class='areas__btn btn-light' label='Подробнее' />
         </div>
       </div>
     </div>
@@ -35,6 +55,7 @@
 <script lang='ts'>
 import { defineComponent } from 'vue';
 import Btn from './Btn.vue';
+import OnlineForm from './OnlineForm.vue';
 //@ts-ignore
 import thumb1 from './assets/img/doc-1.thumb.png'
 //@ts-ignore
@@ -44,6 +65,7 @@ import thumb2 from './assets/img/doc-2.thumb.png'
 export default defineComponent({
   components: {
     'Btn': Btn,
+    'OnlineForm': OnlineForm,
   },
   props: [],
   data() {
@@ -71,6 +93,23 @@ export default defineComponent({
         { label: 'Юридическое&nbsp;сопровождение' },
         { label: 'Разработка&nbsp;документации' },
         { label: 'Лицензирование' },
+      ],
+      resources: [
+        { icon: 'telegram', label: 'Telegram',
+          link: 'https://t.me/grschool'
+        },
+        { icon: 'vk', label: 'ВКонтакте', 
+          link: 'https://vk.com/grand_school_official'
+        },
+        { icon: 'grand', label: 'GrandClass.net',
+          link: 'https://grandclass.net'
+        },
+        { icon: 'zen', label: 'Дзен',
+          link: 'https://zen.yandex.ru/id/618a2c6f54f182703b46e021'
+        },
+        { icon: 'yt', label: 'YouTube',
+          link: 'https://www.youtube.com/channel/UCoCr94xh_Pi-zHr1m0EdbAA'
+        },
       ],
     }
   }
---
+++ src/OnlineForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class='online-form'>
+    <div class='online-form__title'>
+      Регистрация на онлайн-мероприятие «Инструктаж ПОД/ФТ/ФРОМУ»
+    </div>
+    <div class='online-form__row online-form__input'>
+      <input type='text' name='members-number' placeholder='Количество участников' />
+    </div>
+    <div class='online-form__row'>
+      <Btn class='online-form__btn btn btn-primary' label='Далее' />
+    </div>
+  </div>
+</template>
+
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+import Btn from './Btn.vue';
+
+
+export default defineComponent({
+  components: {
+    'Btn': Btn,
+  },
+})
+</script>
